ab5fb39481b9f02e4b155b8798d8d0ae72c843e4
Adam Spontarelli <adam@vector-space.org>
2018-08-21 15:14:47 -0400

    pong.ino now contains test functions in order to test hardware

--- pong.ino
+++ pong.ino
@@ -81,420 +81,237 @@ void setup() {
   pinMode(PinD, IPINMODE);
 
   pinMode(statusLED, OUTPUT);
-
-  enableInterrupt(PinA, pinChangeISR, CHANGE); // Set up pin-change interrupts
-  enableInterrupt(PinB, pinChangeISR, CHANGE);
-  enableInterrupt(PinC, pinChangeISR, CHANGE); // Set up pin-change interrupts
-  enableInterrupt(PinD, pinChangeISR, CHANGE);
   
   abOld = P1_ypos = old_P1_ypos = cdOld = P2_ypos = old_P2_ypos = 0;
 
   // Servos attach to PWM pins
-  P1_yservo.attach(3); 
-  P1_xservo.attach(5);  
-  P2_yservo.attach(6); 
-  P2_xservo.attach(9); 
+  P1_xservo.attach(3); 
+  P1_yservo.attach(5);  
+  P2_yservo.attach(9); 
+  P2_xservo.attach(6); 
   ball_yservo.attach(11); 
   ball_xservo.attach(10); 
 
   pinMode(P1_button_pin, INPUT);
   pinMode(P2_button_pin, INPUT);
+
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+
    
   Serial.begin(115200);
   Serial.println("Starting Pong");
 }
 
 
-// On interrupt, read input pins, compute new state, and adjust P1_ypos
-void pinChangeISR() {
-  enum { upMask = 0x66, downMask = 0x99 };
-  byte abNew = (digitalRead(PinA) << 1) | digitalRead(PinB);
-  byte cdNew = (digitalRead(PinC) << 1) | digitalRead(PinD);
-
-  byte criterion = abNew^abOld;
-  byte criterion2 = cdNew^cdOld;
-
-  
-  if (criterion==1 || criterion==2) {
-    if (upMask & (1 << (2*abOld + abNew/2)))
-      P1_ypos++;
-    else P1_ypos--;       // upMask = ~downMask
-  }
-  abOld = abNew;        // Save new state
-
-
-  if (criterion2==1 || criterion2==2) {
-    if (upMask & (1 << (2*cdOld + cdNew/2)))
-      P2_ypos++;
-    else P2_ypos--;       // upMask = ~downMask
-  }
-  cdOld = cdNew;        // Save new state
-}
-
-
 
 void loop() {
-  /*
-    Calibrate
-    Start game
-    move ball
-    check for collision with walls or paddles
-    show score
-   */
-  counter += 1;
-
-
+  digitalWrite(A2, HIGH);
+  digitalWrite(A3, HIGH);
+  digitalWrite(A4, HIGH);
+  //drawCircle();
+  drawOctogon();
+  //drawRectangle();
+  drawLine();
   
+  //testRange();
+  
+}
+void center(){
+  int minpos = 1000;
+  int maxpos = 2000;
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+  digitalWrite(A4, LOW);
+  P1_xservo.writeMicroseconds((minpos+maxpos) / 2);
+  P1_yservo.writeMicroseconds((minpos+maxpos) / 2);
+  P2_xservo.writeMicroseconds((minpos+maxpos) / 2);
+  P2_yservo.writeMicroseconds((minpos+maxpos) / 2);
+  ball_xservo.writeMicroseconds((minpos+maxpos) / 2);
+  ball_yservo.writeMicroseconds((minpos+maxpos) / 2);
+  delay(1000);
+}
+void testRange(){
+  int minpos = 1000;
+  int maxpos = 2000;
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+  digitalWrite(A4, LOW);
+  P1_xservo.writeMicroseconds(minpos);
+  P1_yservo.writeMicroseconds(minpos);
+  P2_xservo.writeMicroseconds(minpos);
+  P2_yservo.writeMicroseconds(maxpos);
+  ball_xservo.writeMicroseconds(minpos);
+  ball_yservo.writeMicroseconds(minpos);
+  delay(1000);
+  P1_xservo.writeMicroseconds(maxpos);
+  P1_yservo.writeMicroseconds(maxpos);
+  P2_xservo.writeMicroseconds(maxpos);
+  P2_yservo.writeMicroseconds(minpos);
+  ball_xservo.writeMicroseconds(maxpos);
+  ball_yservo.writeMicroseconds(maxpos);
+  delay(1000);
+}
 
-  P1_button_state = digitalRead(P1_button_pin);
-  P2_button_state = digitalRead(P2_button_pin);
-  // Check for simultaneous button press for 5 sec
-  bool buttonsPressed = true;
-  if (P1_button_state == 0 && P2_button_state == 0){
-    for(int i=0; i<10; i++){
-      if (digitalRead(P1_button_pin) == 1 || digitalRead(P2_button_pin) == 1){
-        buttonsPressed = false;
-        break;
+void drawOctogon(){
+  int gameWidth = 800;
+  int gameHeight = 600;
+  unsigned int radius = gameWidth * 0.05;
+
+  int x = 1500;
+  int y = 1300;
+  int thedelay = 30;
+
+  ball_xservo.writeMicroseconds(x);
+  ball_yservo.writeMicroseconds(y);
+  delay(2000);
+
+    for(int n=0;n<5;n++){
+      ball_yservo.writeMicroseconds(y+10);
+      delay(thedelay);
+      ball_xservo.writeMicroseconds(x+10);
+      ball_yservo.writeMicroseconds(y+20);
+      delay(thedelay);
+      ball_xservo.writeMicroseconds(x+20);
+      delay(thedelay);
+      ball_xservo.writeMicroseconds(x+30);
+      ball_yservo.writeMicroseconds(y+10);
+      delay(thedelay);
+      ball_yservo.writeMicroseconds(y);
+      delay(thedelay);
+      ball_xservo.writeMicroseconds(x+20);
+      ball_yservo.writeMicroseconds(y-10);
+      delay(thedelay);
+      ball_xservo.writeMicroseconds(x+10);
+      delay(thedelay);
+      ball_xservo.writeMicroseconds(x);
+      ball_yservo.writeMicroseconds(y);
+      delay(thedelay);
+  }
+}
+
+
+
+
+void drawCircle(){
+  int gameWidth = 800;
+  int gameHeight = 600;
+  unsigned int radius = gameWidth * 0.05;
+
+  int x = 1500;
+  int y = 1500;
+  int thedelay;
+  int j;
+
+  ball_xservo.writeMicroseconds(x);
+  ball_yservo.writeMicroseconds(y);
+  delay(2000);
+
+    for(int n=0;n<10;n++){
+
+       for(int i = x - radius; i < (x + radius); i++)
+       {
+          j = y + sqrt((radius * radius) - ((i - x) * (i - x)));
+          ball_xservo.writeMicroseconds(i);
+          ball_yservo.writeMicroseconds(j);
+          delay(3);
+       }
+       
+       for(int i = x + radius; i > x - radius; i--)
+       {
+          j = y - sqrt((radius * radius) - ((i - x) * (i - x)));
+          ball_xservo.writeMicroseconds(i);
+          ball_yservo.writeMicroseconds(j);
+          delay(3);
+
+       }  
+    }
+}
+
+
+// draw a rectangle using vertical zigzags
+void drawLine(){
+  int gameWidth = 800;
+  int gameHeight = 600;
+  int height=gameHeight * 0.15;
+  int width= height * 0.21;
+  int miny = 1100;
+  int minx = 1000;
+  int maxy=miny+height;
+  int maxx=minx+width;
+  int thedelay;
+
+  P1_xservo.writeMicroseconds(minx);
+  P1_yservo.writeMicroseconds(miny);
+  delay(2000);
+  byte dir = 0;
+  for(int n=0;n<3;n++){
+    for(int i=1; i<=width; i++){
+      if(dir == 0){
+        for(int j=0; j<height; j++){
+           P1_yservo.writeMicroseconds(miny+j);
+           thedelay = max(2000-30*(height-j), 200);
+            Serial.print(j);
+            Serial.print(", ");
+            Serial.println(thedelay);
+            delayMicroseconds(thedelay);
+        }
+        dir = 1;
       }
-      delay(250);
+      else{
+        for(int j=0; j<height; j++){
+           P1_yservo.writeMicroseconds(maxy-j);
+           thedelay = max(2000-30*(height-j), 200);
+           delayMicroseconds(thedelay);
+        }
+        dir = 0;
+      }
     }
-    if (buttonsPressed == true){
-      //calibrate();
-      drawRectangle();
-    }
-  }
-
-  if (start_game){
-  P1_yservo.write(P1_ypos);  
-  P2_yservo.write(P2_ypos);
-  P1_xservo.write(0);
-  P2_xservo.write(display_width);
-
-  // Update paddle positions
-  if (old_P1_ypos != P1_ypos) {
-    Serial.print(millis());
-    Serial.print("  ");
-    Serial.print(P1_ypos);
-    Serial.print("  ");
-    Serial.println(P2_ypos);
-    if (P1_ypos < 0){
-      P1_ypos = 0;
-    }
-    else if (P1_ypos > 180){
-      P1_ypos = 180;
-    }
-        old_P1_ypos = P1_ypos;
-
-  }
-  if (old_P2_ypos != P2_ypos) {
-    Serial.print(millis());
-    Serial.print("  ");
-    Serial.print(P1_ypos);
-    Serial.print("  ");
-    Serial.println(P2_ypos);
-    if (P2_ypos < 0){
-      P2_ypos = 0;
-    }
-    else if (P2_ypos > 180){
-      P2_ypos = 180;
-    }
-        old_P2_ypos = P2_ypos;
-  }
-
-
-  drawPaddle();
-  
-  // move the ball every so many cycles
-  // this prevents other parts of the code from being slowed down in order to slow the ball
-  if (counter % game_speed == 0){
-    moveBall();
-  }
-  
-  checkCollision();
-  }
-
-
-  // DEBUGGING
-  Serial.print("ball x,y: ");
-  Serial.print(ball_x);
-  Serial.print(", ");
-  Serial.println(ball_y);
-  Serial.print("P1 ymin,ymax: ");
-  Serial.print(P1_paddle_bottom);
-  Serial.print(", ");
-  Serial.println(P1_paddle_top);
-  Serial.print("P2 ymin,ymax: ");
-  Serial.print(P2_paddle_bottom);
-  Serial.print(", ");
-  Serial.println(P2_paddle_top);
-  Serial.print("Score: ");
-  Serial.print(P1_score);
-  Serial.print(" : ");
-  Serial.println(P2_score);
-
-  
-  /* delay(200); */
-}
-
-
-void drawPaddle(){
-  /*
-   * Draw a rectangle with the servo
-   */
-  P1_paddle_bottom = P1_ypos - (paddle_height / 2);
-  P1_paddle_top = P1_ypos + (paddle_height / 2);
-  P2_paddle_bottom = P2_ypos - (paddle_height / 2);
-  P2_paddle_top = P2_ypos + (paddle_height / 2);
-
-  // draw p1 paddle
-  for (int pos = 0; pos <= paddle_height; pos += 1) { 
-    P1_yservo.write(P1_ypos + pos);           
-  }
-  for (int pos = 0; pos <= paddle_width; pos += 1) { 
-    P1_xservo.write(P1_xpos + pos);      
-  }
-  for (int pos = 0; pos <= paddle_height; pos += 1) { 
-    P1_yservo.write(P1_ypos + paddle_height - pos);             
-  }
-  for (int pos = 0; pos <= paddle_width; pos += 1) { 
-    P1_xservo.write(P1_xpos + paddle_width - pos);              
-  }
-
-  // draw p2 paddle
-  for (int pos = 0; pos <= paddle_height; pos += 1) { 
-    P2_yservo.write(P2_ypos + pos);           
-  }
-  for (int pos = 0; pos <= paddle_width; pos += 1) { 
-    P2_xservo.write(P2_xpos + pos);      
-  }
-  for (int pos = 0; pos <= paddle_height; pos += 1) { 
-    P2_yservo.write(P2_ypos - pos);             
-  }
-  for (int pos = 0; pos <= paddle_width; pos += 1) { 
-    P2_xservo.write(P2_xpos - pos);              
   }
 }
 
-
-void moveBall(){
-  /*
-    Set the ball's new position, speed, and direction
-   */
-  Serial.println(ball_dir);
-  Serial.println(ball_speed);
-  Serial.print("ystep = ");
-  Serial.println(ball_speed * sin(ball_dir * 1000 / 57296));
-  Serial.print("xstep = ");
-  Serial.println(ball_speed * cos(ball_dir * 1000 / 57296));
-  Serial.print("old x,y = ");
-  Serial.print(ball_x);
-  Serial.print(", ");
-  Serial.println(ball_y);
-  ball_y = ball_y + round(ball_speed * sin(ball_dir * 1000 / 57296)); // deg to rad
-  ball_x = ball_x + round(ball_speed * cos(ball_dir * 1000 / 57296));
-  Serial.print("new x,y = ");
-  Serial.print(ball_x);
-  Serial.print(", ");
-  Serial.println(ball_y);
-  ball_yservo.write(ball_y);  
-  ball_xservo.write(ball_x);
-}
-
-void checkCollision(){
-  
-  
-  // Bounce off top and bottom
-  if (ball_y >= ymax || ball_y <= ymin){
-    ball_dir = 360 - ball_dir;
-    Serial.println("BOUNCE top/bottom!");
-    return;
-  }
-
-  // Bounce off P1 paddle
-  if (ball_x == P1_xpos){
-    if (ball_y >= P1_paddle_bottom && ball_y <= P1_paddle_top){
-      ball_dir = 360 - ball_dir;
-      Serial.println("BOUNCE off P1!");
-      return;
-    }
-  }
-  // Bounce off P2 paddle
-  if (ball_x == P2_xpos){
-    if (ball_y >= P2_paddle_bottom && ball_y <= P2_paddle_top){
-      ball_dir = 360 - ball_dir;
-      Serial.println("BOUNCE off P2!");
-      return;
-    }
-  }
-  
-  // Scoring
-  if (ball_x < P1_xpos){
-    P2_score++;
-    Serial.println("P2 SCORE!");
-    resetBoard();
-  }
-  else if (ball_x > P2_xpos){
-    P1_score++;
-    Serial.println("P1 SCORE!");
-    resetBoard();
-  }
-
-  if (P1_score == 3 || P2_score == 3){
-    gameOver();
-  }
-}
-
-
-void drawScore(){
-  /*
-    Use P1 laser to draw P1's score
-    Use P2 laser to draw P2's score
-   */
-}
-
-
-void gameOver(){
-  Serial.println("GAME OVER");
-  drawScore();
-  delay(4000);
-  start();
-}
-
-void resetBoard(){
-  drawScore();
-  ball_x = display_width / 2;
-  ball_y = display_height / 2;
-  P1_ypos = display_height / 2;
-  P2_ypos = display_height / 2;
-  delay(3000);
-}
-
-
-/* void UpdateScore(){ */
-/* } */
 
 void drawRectangle(){
-  int miny = 550;
-  int minx = 600;
-  int maxy=700;
-  int maxx=650;
-  for(int j=0;j<50;j++){
-    for(int i=1;i<=maxx;i++){
-      ball_xservo.writeMicroseconds(i);
-      delayMicroseconds(5);
+  int gameWidth = 800;
+  int gameHeight = 600;
+  int height=gameHeight * 0.15;
+  int width= height * 0.21;
+  int miny = 1500;
+  int minx = 1500;
+  int maxy=miny+height;
+  int maxx=minx+width;
+  int thedelay;
+
+  ball_xservo.writeMicroseconds(minx);
+  ball_yservo.writeMicroseconds(miny);
+  delay(1000);
+  for(int j=0;j<5;j++){
+    for(int i=1; i<=width; i++){
+      ball_xservo.writeMicroseconds(minx+i);
+      thedelay = max(1000-40*(width-i), 200);
+      Serial.print(i);
+      Serial.print(", ");
+      Serial.println(thedelay);
+      delayMicroseconds(thedelay);
     }
-    delay(90);
-    for(int i=1;i<=maxy;i++){
-      ball_yservo.writeMicroseconds(i);
-      delayMicroseconds(5);
+    for(int i=1; i<=height; i++){
+      ball_yservo.writeMicroseconds(miny+i);
+      thedelay = max(1000-40*(width-i), 200);
+      delayMicroseconds(thedelay);
     }
-    delay(90);
-    for(int i=maxx-1; i>=0 ; i--){
-      ball_xservo.writeMicroseconds(i);
-      delayMicroseconds(5);
+    for(int i=1; i<=width; i++){
+      ball_xservo.writeMicroseconds(maxx-i);
+      thedelay = max(1000-40*(width-i), 200);
+      delayMicroseconds(thedelay);
     }
-    delay(90);
-    for(int i=maxy-1;i>=0;i--){
-      ball_yservo.writeMicroseconds(i);
-      delayMicroseconds(5);
+    for(int i=1; i<=height; i++){
+      ball_yservo.writeMicroseconds(maxy-i);
+      thedelay = max(1000-40*(width-i), 200);
+      delayMicroseconds(thedelay);
     }
-    delay(90);
   }
 }
 
 
-void calibrate(){
-  /*
-   * Read the rotary encoder position and use those values to modify the display_width and display_height values.
-   */
-  digitalWrite(statusLED, HIGH);
-  Serial.println("Entering calibration mode");
-  delay(1500);
-  bool stop = false;
-  int xcount = 0;
-  int xchange = 1;
-  int ycount = 0;
-  int ychange = 1;
-  while (stop == false){
-    Serial.print("Width, height: ");
-    Serial.print(display_width);
-    Serial.print(", ");
-    Serial.println(display_height);
 
-    Serial.print(xcount);
-    Serial.print(", ");
-    Serial.println(ycount);
-    
-    display_width = P1_ypos;
-    display_height = P2_ypos;
-
-    // Sweep height and width back and forth
-    // 90 is center
-    P1_xservo.write(xcount);
-    P2_yservo.write(ycount);
-    if (xcount > display_width || xcount < 0){
-      xchange = xchange * -1;
-    }
-    xcount += xchange;
-    if (ycount > display_height || ycount < 0){
-      ychange = ychange * -1;
-    }
-    ycount += ychange;
-
-    if (xchange == 1){
-      digitalWrite(statusLED, HIGH);
-    }
-    else{
-      digitalWrite(statusLED, LOW);  
-    }
-
-    delay(2);
- 
-      if (digitalRead(P1_button_pin) == 0){
-        Serial.println("CALIBRATION DONE");
-        digitalWrite(statusLED, LOW);
-        delay(1000);
-        stop = true;
-        
-      }
-
-      //move ball around the bounding area for testing
-      for(int i=0; i<display_width; i++){
-        ball_xservo.write(i);
-      }
-      for(int j=0; j<display_width; j++){
-        ball_yservo.write(j);
-      }
-    }
-
-  // position the ball
-  ball_x = display_width / 2;
-  ball_y = display_height / 2;
-
-  // position paddles
-  P1_xpos = 0;
-  P2_xpos = display_width;
-  P1_ypos = display_height / 2;
-  P2_ypos = display_height / 2;
-  ymin = 0;
-  ymax = display_height;
-
-  calibrated = true;
-  
-  start();
-}
-
-void start(){
-  Serial.println("Starting Game");
-  delay(500);
-  P1_score = 0;
-  P2_score = 0;
-  ball_speed = 2;
-  ball_dir = random(0,360);
-  digitalWrite(statusLED, 0);
-  start_game = true;
-  while (digitalRead(A0) == 1){
-    delay(40);
-  }
-  digitalWrite(statusLED, 1);
-}
